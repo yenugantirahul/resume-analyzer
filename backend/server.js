c5b115452d9d961131fc15b9bcd209121545fbd8
Yenuganti Rahul <yenugantirahul@gmail.com>
2025-09-18 19:04:43 +0530

    updated the cors origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://resume-analyzer-project.vercel.app",
     credentials: true,
   })
 );
